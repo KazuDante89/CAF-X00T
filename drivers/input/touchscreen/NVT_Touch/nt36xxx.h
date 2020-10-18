74908911e0371c48bf611052c279db1434076c97
Syarif Ibnu <syarifibnuzaelani57@gmail.com>
2020-10-18 09:50:19 +0700

    NVT-ts: Remove earlysuspend support
    Truthfully, hardly any modern devices support earlysuspend anymore
    instead of FB. We can just remove this filler code.
    
    Signed-off-by: Yasir-Siddiqui <www.mohammad.yasir.s@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: Syarif Ibnu <syarifibnuzaelani57@gmail.com>

--- drivers/input/touchscreen/NVT_Touch/nt36xxx.h
+++ drivers/input/touchscreen/NVT_Touch/nt36xxx.h
@@ -31,9 +31,6 @@
 #include <linux/regulator/consumer.h>
 //#include <linux/debugfs.h>
 /* Huaqin add by yuexinghan for ITO test end */
-#ifdef CONFIG_HAS_EARLYSUSPEND
-#include <linux/earlysuspend.h>
-#endif
 
 #define NVT_DEBUG 0
 
@@ -144,11 +141,7 @@ struct nvt_ts_data {
 	struct delayed_work nvt_fwu_work;
 	uint16_t addr;
 	int8_t phys[32];
-#if defined(CONFIG_FB)
 	struct notifier_block fb_notif;
-#elif defined(CONFIG_HAS_EARLYSUSPEND)
-	struct early_suspend early_suspend;
-#endif
 	uint8_t fw_ver;
 	uint8_t x_num;
 	uint8_t y_num;
